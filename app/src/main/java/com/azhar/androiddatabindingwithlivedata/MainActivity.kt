7d88e6cf604b28ccdfbf76e0d33efab8f191f02f
MD. Azharul Islam <36479711+azharcse14@users.noreply.github.com>
2021-10-04 11:34:13 +0600

    update

--- app/src/main/java/com/azhar/androiddatabindingwithlivedata/MainActivity.kt
+++ app/src/main/java/com/azhar/androiddatabindingwithlivedata/MainActivity.kt
@@ -2,10 +2,18 @@ package com.azhar.androiddatabindingwithlivedata
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil.setContentView
+import com.azhar.androiddatabindingwithlivedata.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = setContentView(this, R.layout.activity_main)
+
+        binding.tvId
+
     }
 }
